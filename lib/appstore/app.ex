7b8b78fb426a71981bf90f2db88274f3cc0c1bf8
Ícaro Ponce <ponceicaro@gmail.com>
2017-06-16 00:02:14 -0300

    Added clean app to the get_details method to use struct

--- lib/appstore/app.ex
+++ lib/appstore/app.ex
@@ -1,9 +1,16 @@
 defmodule AppStore.App do
+  import AppStore.Common
+
   @base_url "https://itunes.apple.com/lookup"
 
   def get_details(bundleId, country \\ "us") do
     url = "#{@base_url}?bundleId=#{bundleId}&country=#{country}"  
-    app = Task.async(fn -> AppStore.Common.request(url) end)
-    Task.await app
+    app = Task.async(fn -> request(url) end)
+    fetched_app = Task.await app 
+    case fetched_app do
+      {:ok, data} -> {:ok, clean_app(List.first(data))}
+      {:error, msg} -> {:error, msg}
+      :error -> :error
+    end
   end
 end
